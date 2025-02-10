dade8524ad80fdffcddb36b0678dd0a03d59645c
Mubaraq Onipede <93594617+Mubarraqqq@users.noreply.github.com>
2025-02-10 01:18:42 +0100

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.edge.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import requests
